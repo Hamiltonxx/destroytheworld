c0e8a25d468abcc71770255e5859a3e39cc88536
Huangguozheng <huangguozheng@licaigc.com>
2017-06-20 00:50:04 +0800

    Initial Commit

---
+++ destroy-the-world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destroy-the-world
+//
+//  Created by Hamilton Huang on 20/06/2017.
+//  Copyright © 2017 Cirray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
